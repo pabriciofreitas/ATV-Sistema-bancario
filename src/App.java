375d4104df2a93eaa674697d82be520e8a6d3411
Pabricio Freitas <pabricio2000@gmail.com>
2021-11-05 20:09:26 -0300

    Definir o que Cada um vai Fazer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+    // List extratoCorrentista = []
+    // List listaDeCorrentista = correntista,atributo - extrado dentro da class
+    // correntista [] ]
+    // Conta fulano = new Conta(nome, a, b)
+    // criar um atributo de class static para gerenciar os número da conta
+    // criar class de extrato(remetente , destinatário, operacões (String))
+
+    // kleber
+    // print("PEssoa Juridica ou PEssoa fisica");
+    // Primeeiro menu - logiin(número da conta e código verificado)
+    // se adm cadastrar novo cliente e listar todos cliente, voltar pro menu de
+    // login e sair do programa
+    // se for cliente operações da conta. voltar pro menu de login e sair do
+    // programa
+
+    // Marcos
+    // pessoa vai fica responsavel por criar class Cliente(atributos, contrustor,
+    // get e set e digita nomes das funcoes)
+    // Cadastro de COnta(clientes e contas): Cadastrar uma conta bancária com
+    // endereço, número, CPF ou CNPJ, nome do titular ou razão social, saldo e
+    // limite.
+
+    // Hevelin
+    // pessoa vai fica responsavel Class Conta
+    //
+  }
 }
