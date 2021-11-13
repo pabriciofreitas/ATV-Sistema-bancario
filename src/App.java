299d074f141c1c68ca3eb715c3976d790d97132f
Pabricio Freitas <pabricio2000@gmail.com>
2021-11-13 10:24:49 -0300

    Kleber: criação do menu

--- src/App.java
+++ src/App.java
@@ -1,8 +1,4 @@
-
-import conta.Cliente;
-import conta.Conta;
-
-
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -10,34 +6,65 @@ import conta.Conta;
  */
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        //cadstro o cliente
-        Cliente primeiroCliente = new  Cliente("Carlos", "785.695.216-32");
-         Cliente SegundoCliente = new  Cliente("Maria", "685.545.656-56");
-        
-        // Na hora de criar uma conta deve se adicionar CC para conta corrente e CP para conta poupança
+  public static void sair() {
 
-      //Instancia a class
-        Conta conta01 = new Conta();
-         Conta conta02= new Conta();
-        
-         //cadstro a conta com os dados do cliente
-        conta01.criarConta(primeiroCliente, "CC");
-        conta02.criarConta(SegundoCliente, "CC");
-        
-        //DESPOISTAR
-        conta01.depsitar(1000);
-        
-        //mostrar conta depois do deposito 
-        conta01.mostrarConta();
-        conta02.mostrarConta();
-        
-       
-        
+    int x = JOptionPane.showConfirmDialog(null, " DESEJA REALMENTE SAIR ? ", "  ", JOptionPane.YES_NO_OPTION);
+    if (x == JOptionPane.YES_OPTION) {
+      JOptionPane.showMessageDialog(null, "PROGRAMA ENCERRADO");
+    } else {
+      login();
     }
-    
+  }
+
+  public static void login() {
+    int a = 0;
+    int b = 0;
+    String id, senha;
+
+    id = JOptionPane.showInputDialog("DIGITE SEU LOGIN:");
+    senha = JOptionPane.showInputDialog("DIGITE SUA SENHA:");
+    if ("admin".equals(id) && "admin".equals(senha)) {
+      a = Integer.parseInt(
+          JOptionPane.showInputDialog(null, "1 - ABRIR CONTA\n" + "2 - IMPRIMIR TODAS AS CONTAS\n" + "3 - SAIR"));
+      {
+        if (a == 3) {
+          sair();
+        }
+      }
+    } else if ("123".equals(id) && "123".equals(senha)) {
+      b = Integer.parseInt(JOptionPane.showInputDialog(null,
+          "1 - SACAR\n" + "2 - DEPOSITAR\n" + "3 - TRANSFERIR\n" + "4 - EXTRATO\n" + "5 - SAIR\n"));
+    }
+    if (b == 5) {
+      sair();
+    }
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    login();
+    /*
+     * cadstro o cliente Cliente primeiroCliente = new Cliente("Carlos",
+     * "785.695.216-32"); Cliente SegundoCliente = new Cliente("Maria",
+     * "685.545.656-56");
+     * 
+     * // Na hora de criar uma conta deve se adicionar CC para conta corrente e CP
+     * para // conta poupança
+     * 
+     * // Instancia a class Conta conta01 = new Conta(); Conta conta02 = new
+     * Conta();
+     * 
+     * // cadstro a conta com os dados do cliente
+     * conta01.criarConta(primeiroCliente, "CC"); conta02.criarConta(SegundoCliente,
+     * "CC");
+     * 
+     * // DESPOISTAR conta01.depsitar(1000);
+     * 
+     * // mostrar conta depois do deposito conta01.mostrarConta();
+     * conta02.mostrarConta();
+     */
+  }
+
 }
