d0772b8c0288bf3c6926191a3be34981ef234024
Pabricio Freitas <pabricio2000@gmail.com>
2021-12-08 20:08:50 -0300

    feat: atualização

--- test.py
+++
@@ -1,9 +0,0 @@
-letra = (input("digite seu sexo em letra:"))
-f = "f"
-m = "m"
-if letra.lower() == f:
-    print("Feminino")
-elif letra.lower() == m:
-    print("Masculino")
-else:
-    print("Sexo Inválido")
